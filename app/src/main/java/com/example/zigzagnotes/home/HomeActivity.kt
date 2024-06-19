a3612111ddf5c5819c895b473952b9036cac2ae5
Vandana Tailor <vandanatailorsmtlabs@gmail.com>
2024-06-19 10:30:56 +0530

    Add Toggel Button

--- app/src/main/java/com/example/zigzagnotes/home/HomeActivity.kt
+++ app/src/main/java/com/example/zigzagnotes/home/HomeActivity.kt
@@ -1,21 +1,33 @@
 package com.example.zigzagnotes.home
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.zigzagnotes.R
+import com.example.zigzagnotes.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding=ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setBottomVisiblity()
     }
+    private fun setBottomVisiblity() {
+        binding.lvAddNotes.setOnClickListener {
+            if(binding.rlAdd.visibility==View.VISIBLE){
+                binding.rlAdd.visibility= View.GONE
+            }else{
+                binding.rlAdd.visibility= View.VISIBLE
+            }
+
+        }
+
+    }
+
+
 }
